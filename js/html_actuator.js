258b5fd20cc77d010df073b8617432f872b9585a
Ryan2144 <70672438+Ryan2144@users.noreply.github.com>
2024-08-29 17:33:35 -0500

    Adjusted tile-super

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -57,7 +57,7 @@ HTMLActuator.prototype.addTile = function (tile) {
   // We can't use classlist because it somehow glitches when replacing classes
   var classes = ["tile", "tile-" + tile.value, positionClass];
 
-  if (tile.value > 32768) classes.push("tile-super");
+  if (tile.value > 63245986) classes.push("tile-super");
 
   this.applyClasses(wrapper, classes);
 
